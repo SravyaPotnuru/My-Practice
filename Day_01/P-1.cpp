654fcb3177254f868330cdb189fb267af53c41ca
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-09-30 20:56:49 +0530

    Create P-1.cpp

---
+++ Day_01/P-1.cpp
@@ -0,0 +1,20 @@
+//Majority Element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       int k=0;
+        int ele=0;
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto i=m.begin();i!=m.end();i++){
+            if(i->second>k){
+                ele=i->first;
+                k=i->second;
+            }
+        }
+        return ele; 
+    }
+};
