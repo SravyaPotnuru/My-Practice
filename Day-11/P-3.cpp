35d5267670397dfecdda8e3c5d758a4cf9784759
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-10-08 23:10:41 +0530

    Create P-3.cpp

---
+++ Day-11/P-3.cpp
@@ -0,0 +1,10 @@
+//Minimum Depth of Binary Tree
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        else
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
