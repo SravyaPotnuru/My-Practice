cb5ff4fe0d45bbeedb5ec753a6b2c45d4ba0c8b0
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-10-08 22:59:32 +0530

    Create P-1.cpp

---
+++ Day-10/P-1.cpp
@@ -0,0 +1,13 @@
+//Pth sum in a BINARY TREE
+
+class Solution {
+public:
+bool solve(TreeNode* root,int sum){
+     if(!root) return false;
+    if( sum==root->val && !root->left && !root->right) return true;
+    return solve(root->left,sum-root->val)||solve(root->right,sum-root->val);
+                                        }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+     return solve(root,targetSum);
+        }
+    };
