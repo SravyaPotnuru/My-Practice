4f06c0dab83ef863a5b801206d45dc7d47cb9268
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-10-10 11:42:56 +0530

    Create P-3.cpp

---
+++ Day-14/P-3.cpp
@@ -0,0 +1,13 @@
+//Insert in a Binary search Tree
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL) return new TreeNode(val);
+        else if(root->val<val)
+        root->right=insertIntoBST(root->right,val);
+        else
+        root->left=insertIntoBST(root->left,val);
+        return root;
+    }
+};
